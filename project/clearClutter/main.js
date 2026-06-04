c56fbbf4619e52ea4de05d3ef54ea6d1cf9b4754
Harsh Bansal <96734004+Harshbansal06@users.noreply.github.com>
2026-06-04 23:37:01 +0530

    clearCluster : To clean your files cluster
    It use fs module of Node.js , to move your files to their dedicated folder, created on the basis of their extensions.

---
+++ project/clearClutter/main.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const mpath = path.join(__dirname,"files")
+
+// console.log(mpath)
+fs.readdir(mpath,  (err, files) => {
+    files.forEach(file => {
+
+        // let k = file.split('.')[1]
+        let k = path.extname(file)
+
+        
+
+
+        const fpath = path.join(path.dirname(mpath), k);
+
+        fs.mkdirSync(fpath, { recursive: true })
+
+        const newpath = path.join(fpath, file)
+        const oldpath = path.join(mpath, file)
+
+        fs.renameSync(oldpath, newpath)
+        
+    });
+
+    fs.rmdir(mpath,(er)=>{
+        console.log(er)
+    })
+})
